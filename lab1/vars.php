b04e6f8b68970db38ebeeea4c6c7e38b71b08b51
vassabi353 <vassabi353@mail.ru>
2025-12-19 14:39:35 +0300

    Update vars.php

--- lab1/vars.php
+++ lab1/vars.php
@@ -15,8 +15,8 @@ $age = 19;
 	<?php
 	echo "Меня зовут: $name<br>";
 	echo "Мне: $age лет<br>";
-	echo "Тип переменной: $name: " . gettype($name) . "<br>";
-	echo "Тип переменной: $age: " . gettype($age) . "<br>";
+	echo 'Тип переменной: $name: ' . gettype($name) . "<br>";
+	echo 'Тип переменной: $age: ' . gettype($age) . "<br>";
 	
 	unset($name);
 	unset($age);
